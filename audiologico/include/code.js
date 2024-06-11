36e978351ea72a8c6823e2bbd867fe3ef30437fd
mfischborn <marcelofischborn@gmail.com>
2024-06-11 20:53:16 -0300

    Add files via upload

---
+++ audiologico/include/code.js
@@ -0,0 +1,35 @@
+function speak(text) {
+    responsiveVoice.speak(text, "Brazilian Portuguese Female");
+}
+
+function setTextToSpeech() {
+    const paragraphs = document.querySelectorAll('.tts'); //from p.tts
+    paragraphs.forEach(p => {
+        p.onclick = () => speak(p.innerText);
+    });
+}
+
+let points = 0;
+
+function checkAnswer(isCorrect, currentElement) {
+    
+    const lastParent = currentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+    const feedback = lastParent.querySelector('.feedback');
+    console.log(feedback);
+    
+    const pointsDisplay = document.getElementById('points');
+    if (isCorrect) {
+        feedback.innerText = 'Correto!';
+        //feedback.style.color = 'green';
+        points++;
+    } else {
+        feedback.innerText = 'Incorreto. Tente novamente!';
+        //feedback.style.color = 'red';
+        points--;
+    }
+    pointsDisplay.innerText = 'Pontos: ' + points;
+    speak(feedback.innerText);
+    
+}
+
+
